bcab54aab4cdf96fc171411c9c694516d3a0e743
Tạ Hữu Bình <binhth.vuejx@gmail.com>
2022-10-17 12:56:39 +0700

    Update applyGraphQL.ts

--- applyGraphQL.ts
+++ applyGraphQL.ts
@@ -1,5 +1,5 @@
 import { graphql, gql } from "./deps.ts";
-import { renderPlaygroundPage, ISettings } from "./graphql-playground-html/render-playground-html.ts";
+import { ISettings } from "./graphql-playground-html/render-playground-html.ts";
 import { makeExecutableSchema } from "./graphql-tools/schema/makeExecutableSchema.ts";
 import { fileUploadMiddleware, GraphQLUpload } from "./fileUpload.ts";
 
@@ -92,74 +92,6 @@ export async function applyGraphQL<T>({
       }
     }
   });
-
-  await router.get(path, async (ctx: any) => {
-    const { request, response } = ctx;
-    if (usePlayground) {
-      // perform more expensive content-type check only if necessary
-      // XXX We could potentially move this logic into the GuiOptions lambda,
-      // but I don't think it needs any overriding
-      const prefersHTML = request.accepts("text/html");
-
-      if (prefersHTML) {
-        const playground = renderPlaygroundPage({
-          endpoint: request.url.origin + path,
-          subscriptionEndpoint: request.url.origin,
-          settings
-        });
-        response.status = 200;
-        response.body = playground;
-        return;
-      }
-    }
-  });
-
-  // await router.get("/", async (ctx) => {
-  //   const { request, response } = ctx;
-  //   const WS = new SubscriptionServer(
-  //     {
-  //       schema,
-  //       execute: execute as any,
-  //       subscribe,
-  //       onConnect: onConnect
-  //         ? onConnect
-  //         : (connectionParams: Object) => ({ ...connectionParams }),
-  //       onDisconnect: onDisconnect,
-  //       onOperation: async (
-  //         message: { payload: any },
-  //         connection: any,
-  //       ) => {
-  //         connection.formatResponse = (value: any) => value;
-
-  //         // connection.formatError = this.requestOptions.formatError;
-  //         let contextResult;
-  //         try {
-  //           contextResult = context ? await context(ctx) : undefined;
-  //         } catch (error) {
-  //           response.status = 200;
-  //           response.body = {
-  //             data: null,
-  //             errors: [{
-  //               message: error.message ? error.message : error,
-  //             }],
-  //           }
-  //         }
-
-  //         return { ...connection, context: contextResult };
-  //       },
-  //       keepAlive: undefined,
-  //       validationRules: undefined
-  //     },
-  //   );
-  // })
-
-  // await router.get("/", async (ctx) => {
-  //   if (ctx.isUpgradable) {
-  //     const socket = await ctx.upgrade();
-  //     socket.send("graphql-ws")
-  //     await chat(socket)
-  //   }
-  // })
 
   return router;
 }
